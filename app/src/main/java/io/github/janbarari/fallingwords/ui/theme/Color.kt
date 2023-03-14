468e562d97f9e3a36ecfb4a9cf35183b99f6b944
janbarari <mehdimjt@gmail.com>
2023-03-14 16:35:47 +0330

    refactor(4): Update compose theme configs

--- app/src/main/java/io/github/janbarari/fallingwords/ui/theme/Color.kt
+++ app/src/main/java/io/github/janbarari/fallingwords/ui/theme/Color.kt
@@ -2,7 +2,5 @@ package io.github.janbarari.fallingwords.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Brand = Color(0xFFFF790F)
+val Black = Color(0xFF000000)
